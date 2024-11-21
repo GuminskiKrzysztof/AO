97d7c2591766c3aac20451cbd1b4cea3db6c7d9c
Dzida <66182012+Dzida-0@users.noreply.github.com>
2024-11-21 14:30:31 +0100

    AO

--- AOA/Aquila.cs
+++ AOA/Aquila.cs
@@ -30,14 +30,18 @@ namespace AOA
         private double s;
         private double U;
         private int r1;
+        public int OOO = 0;
         private double x;
         private double y;
         private double sigma;
         private Random rand;
         Func<double[], double> testFunction;
 
+        private int rcount;
+        private double r;
+
         public Aquila(Func<double[], double> myFuncion, int dim_in, int iterations_in, double[][] range_in, int population_size_in, double alpha_in, double beta_in,
-            double delta_in, double omega_in, int D_in, double U_in, double s_in,int r1_in)
+            double delta_in, double omega_in, int D_in, double U_in, double s_in, int r1_in)
         {
             Name = "AOA";
             populationSize = population_size_in;
@@ -49,10 +53,12 @@ namespace AOA
             beta = beta_in;
             delta = delta_in;
             omega = omega_in;
+            rcount = 0;
             U = U_in;
             s = s_in;
             r1 = r1_in;
-            sigma = ((SpecialFunctions.Gamma(1+ beta)) * Math.Sin(Math.PI * beta / 2)) / ((SpecialFunctions.Gamma((1+beta)/2)) * beta * Math.Pow(2,(beta - 1)/2));
+            OOO = 0;
+            sigma = ((SpecialFunctions.Gamma(1 + beta)) * Math.Sin(Math.PI * beta / 2)) / ((SpecialFunctions.Gamma((1 + beta) / 2)) * beta * Math.Pow(2, (beta - 1) / 2));
             rand = new Random();
 
         }
@@ -77,110 +83,23 @@ namespace AOA
         private void UpdatePositions(int iteration)
         {
             double[] newPosition = new double[dimentions];
-          
-            if (iteration < iterations*2/3)
+
+            double[] meanDimentions = new double[dimentions];
+            for (int i = 0; i < dimentions; i++)
             {
-                    if (rand.NextDouble() <= 0.5)
-                    {
-                        double[] meanDimentions = new double[dimentions];
-                        for (int i = 0; i < dimentions; i++)
-                        {
-                            meanDimentions[i] = 0.0;
-                            for (int j = 0; j < populationSize; j++)
-                            {
-                                meanDimentions[i] += population[j][i];
-                            }
-                            meanDimentions[i] /= populationSize;
-                        }
-                        for (int i = 0; i < populationSize; i++)
-                        {
-                            for (int j = 0; j < dimentions; j++)
-                            {
-                            int do_control = 0;
-                            do
-                            {
-                                newPosition[j] = XBest[j] * (1 - (iteration / iterations)) + (meanDimentions[j] - XBest[j] * rand.NextDouble());
-                                do_control++;
-                            } while (! (range[j][0] < newPosition[j] && newPosition[j] < range[j][1])&& do_control < 100);
-                            if (do_control == 100)
-                            {
-                                newPosition[j] = population[i][j];
-                            }
-
-
-
-                        }
-
-                            if (testFunction(newPosition) < functionValue[i])
-                            {
-                                population[i] = newPosition;
-                                functionValue[i] = testFunction(newPosition);
-
-
-                                if (testFunction(newPosition) < FBest)
-                                {
-                                XBest = (double[])newPosition.Clone();
-                                FBest = testFunction(XBest);
-                                }
-                            }    
-                        }
-                    
-
-                    }
-                    else
-                    {
-                        int randomPopulation = rand.Next(populationSize);
-                        int r1 = rand.Next(20);
-                    for (int i = 0; i < populationSize; i++)
-                        {
-                            for (int j = 0; j < dimentions; j++)
-                            {
-                            int do_control = 0;
-                            do {
-                            double theta = -omega * rand.Next(dimentions) + ((3 * Math.PI) / 2);
-                            double r = r1 + U * rand.Next(dimentions);
-                            x = r * Math.Sin(theta);
-                            y = r * Math.Cos(theta);
-                            double levy = s * rand.NextDouble() * sigma / Math.Pow(rand.NextDouble(), 1 / beta); // difrent for every dim ???? 
-                            newPosition[j] = XBest[j] * levy + population[randomPopulation][j] + (y-x) * rand.NextDouble();
-                                do_control++;
-                            } while (!(range[j][0] < newPosition[j] && newPosition[j] < range[j][1]) && do_control<100) ;
-                            if (do_control == 100)
-                            {
-                                newPosition[j] = population[i][j];
-                            }
-                        }
-
-                            if (testFunction(newPosition) < functionValue[i])
-                            {
-                                population[i] = newPosition;
-                                functionValue[i] = testFunction(newPosition);
-
-
-                                if (testFunction(newPosition) < FBest)
-                                {
-                                XBest = (double[])newPosition.Clone();
-                                FBest = testFunction(XBest);
-                                }
-                            }
-                        }
-                    }
+                meanDimentions[i] = 0.0;
+                for (int j = 0; j < populationSize; j++)
+                {
+                    meanDimentions[i] += population[j][i];
+                }
+                meanDimentions[i] /= populationSize;
             }
-            else
+
+            if (iteration < iterations * 2 / 3)
             {
                 if (rand.NextDouble() <= 0.5)
                 {
-                    double[] meanDimentions = new double[dimentions];
-                    for (int i = 0; i < dimentions; i++)
-                    {
-                        meanDimentions[i] = 0;
-                        for (int j = 0; j < populationSize; j++)
-                        {
-                            meanDimentions[i] += population[j][i];
-                        }
-                        meanDimentions[i] /= populationSize;
-                    }
-
+                   
                     for (int i = 0; i < populationSize; i++)
                     {
                         for (int j = 0; j < dimentions; j++)
@@ -188,12 +107,70 @@ namespace AOA
                             int do_control = 0;
                             do
                             {
+                                newPosition[j] = XBest[j] * (1 - (iteration / iterations)) + (meanDimentions[j] - XBest[j] * rand.NextDouble());
                                 do_control++;
-                                newPosition[j] = (XBest[j] - meanDimentions[j]) * alpha - rand.NextDouble() + ((range[j][1] - range[j][0]) * rand.NextDouble() + range[j][0]) * delta;
-                            } while (!(range[j][0] < newPosition[j] && newPosition[j] < range[j][1]) && do_control < 100);
-                            if (do_control == 100)
+                            } while (!(range[j][0] <= newPosition[j] && newPosition[j] <= range[j][1]) && do_control < 1000);
+                            if (do_control == 1000)
                             {
-                                newPosition[j] = population[i][j];
+                                newPosition[j] = meanDimentions[j];
+                                //OOO += 1;
+                            }
+
+
+
+                        }
+
+                        if (testFunction(newPosition) < functionValue[i])
+                        {
+                            population[i] = newPosition;
+                            functionValue[i] = testFunction(newPosition);
+
+
+                            if (testFunction(newPosition) < FBest)
+                            {
+                                XBest = (double[])newPosition.Clone();
+                                FBest = testFunction(XBest);
+                            }
+                        }
+                    }
+
+
+                }
+                else
+                {
+                    
+                    int r1 = rand.Next(20);
+                    double[] levyD = new double[dimentions];
+                    for (int j = 0; j < dimentions; j++)
+                    {
+                         levyD[j] = s * rand.NextDouble() * sigma / Math.Pow(rand.NextDouble(), 1 / beta);
+                    }
+                    if (rcount == 0)
+                    {
+                        r = r1 + U * rand.Next(dimentions);
+                        rcount = rand.Next(10);
+                    }
+                    rcount--;
+                    for (int i = 0; i < populationSize; i++)
+                    {
+                        double theta = -omega * rand.Next(dimentions) + ((3 * Math.PI) / 2);
+                        
+                        x = r * Math.Sin(theta);
+                        y = r * Math.Cos(theta);
+                       // difrent for every dim ???? 
+                        int randomPopulation = rand.Next(populationSize);
+                        for (int j = 0; j < dimentions; j++)
+                        {
+                            int do_control = 0;
+                            do
+                            {
+                                newPosition[j] = XBest[j] * levyD[j] + population[randomPopulation][j] * ((rand.Next(20) / 10) - 1) +  (y - x) * ((rand.Next(20)/10)-1);
+                                do_control++;
+                            } while (!(range[j][0] <= newPosition[j] && newPosition[j] <= range[j][1]) && do_control < 1000);
+                            if (do_control == 1000)
+                            {
+                                newPosition[j] = meanDimentions[j];
+                                OOO += 1;
                             }
                         }
 
@@ -211,49 +188,89 @@ namespace AOA
                         }
                     }
                 }
-                    else
+            }
+            else
+            {
+                if (rand.NextDouble() <= 0.5)
+                {
+                    
+
+                    for (int i = 0; i < populationSize; i++)
                     {
-                    double qf = Math.Pow(iteration, (2 * rand.NextDouble() - 1) / Math.Pow(1 - iterations, 2));
-                    int  r1 = rand.Next(20);
-                       
-                        double g2 = 2 * (1 - iteration/iterations);
-                        
-                        for (int i = 0; i < populationSize; i++)
+                        for (int j = 0; j < dimentions; j++)
                         {
-                            for (int j = 0; j < dimentions; j++)
-                            {
                             int do_control = 0;
-                            do { 
-                            double g1 = 2 * rand.NextDouble() - 1;
-                            
-                            double theta = -omega * rand.Next(dimentions) + ((3 * Math.PI) / 2);
-                                double r = r1 + U * rand.Next(dimentions);
-                                x = r * Math.Sin(theta);
-                                y = r * Math.Cos(theta);
-                            double levy = s * rand.NextDouble() * sigma / Math.Pow(rand.NextDouble(), 1 / beta); // difrent for every dim ???? 
-                                newPosition[j] = qf * XBest[j] - g1 * population[i][j] * rand.NextDouble() - g2 * levy + rand.NextDouble() * g1;
-                                do_control++;
-                            } while (!(range[j][0] < newPosition[j] && newPosition[j] < range[j][1]) && do_control<100);
-                            if (do_control == 100)
+                            do
                             {
-                                newPosition[j] = population[i][j];
+                                do_control++;
+                                newPosition[j] = (XBest[j] - meanDimentions[j]) * alpha - rand.NextDouble() + ((range[j][1] - range[j][0]) * rand.NextDouble() + range[j][0]) * delta;
+                            } while (!(range[j][0] <= newPosition[j] && newPosition[j] <= range[j][1]) && do_control < 1000);
+                            if (do_control == 1000)
+                            {
+                                newPosition[j] = meanDimentions[j];
+                                //OOO += 1;
                             }
                         }
 
-                            if (testFunction(newPosition) < functionValue[i])
+                        if (testFunction(newPosition) < functionValue[i])
+                        {
+                            population[i] = newPosition;
+                            functionValue[i] = testFunction(newPosition);
+
+
+                            if (testFunction(newPosition) < FBest)
                             {
-                                population[i] = newPosition;
-                                functionValue[i] = testFunction(newPosition);
-
-
-                                if (testFunction(newPosition) < FBest)
-                                {
                                 XBest = (double[])newPosition.Clone();
                                 FBest = testFunction(XBest);
-                                }
                             }
                         }
                     }
+                }
+                else
+                {
+                    double qf = Math.Pow(iteration, (2 * rand.NextDouble() - 1) / Math.Pow(1 - iterations, 2));
+                    int r1 = rand.Next(20);
+
+                    double g2 = 2 * (1 - iteration / iterations);
+
+                    for (int i = 0; i < populationSize; i++)
+                    {
+                        for (int j = 0; j < dimentions; j++)
+                        {
+                            int do_control = 0;
+                            do
+                            {
+                                double g1 = 2 * rand.NextDouble() - 1;
+
+                                double theta = -omega * rand.Next(dimentions) + ((3 * Math.PI) / 2);
+                                double r = r1 + U * rand.Next(dimentions);
+                                x = r * Math.Sin(theta);
+                                y = r * Math.Cos(theta);
+                                double levy = s * rand.NextDouble() * sigma / Math.Pow(rand.NextDouble(), 1 / beta); // difrent for every dim ???? 
+                                newPosition[j] = qf * XBest[j] - g1 * population[i][j] * rand.NextDouble() - g2 * levy + rand.NextDouble() * g1;
+                                do_control++;
+                            } while (!(range[j][0] <= newPosition[j] && newPosition[j] <= range[j][1]) && do_control < 1000);
+                            if (do_control == 1000)
+                            {
+                                newPosition[j] = meanDimentions[j];
+                                //OOO += 1;
+                            }
+                        }
+
+                        if (testFunction(newPosition) < functionValue[i])
+                        {
+                            population[i] = newPosition;
+                            functionValue[i] = testFunction(newPosition);
+
+
+                            if (testFunction(newPosition) < FBest)
+                            {
+                                XBest = (double[])newPosition.Clone();
+                                FBest = testFunction(XBest);
+                            }
+                        }
+                    }
+                }
             }
         }
 
@@ -263,22 +280,22 @@ namespace AOA
             XBest = population[0];
             FBest = testFunction(population[0]);
             functionValue = new double[populationSize];
-            for(int i =0; i < populationSize;i++)
+            for (int i = 0; i < populationSize; i++)
             {
                 functionValue[i] = testFunction(population[i]);
             }
             for (int i = 0; i < iterations; i++)
             {
-                Console.WriteLine(i < iterations * 2 / 3);
-                Console.WriteLine(FBest);
-                foreach(double d in XBest)
+                //Console.WriteLine(i < iterations * 2 / 3);
+                //Console.WriteLine(FBest);
+                //foreach (double d in XBest)
                 {
-                    Console.Write(" " + d.ToString());
+                  //  Console.Write(" " + d.ToString());
                 }
-                Console.WriteLine();
+                //Console.WriteLine();
                 UpdatePositions(i);
-                
-               
+
+
             }
             return FBest;
         }
--- AOA/Program.cs
+++ AOA/Program.cs
@@ -1,22 +1,50 @@
 ﻿using System;
+
 namespace AOA
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            double[][] range = new double[10][];
-            for (int t = 0; t < 10; t++)
-                range[t] = new double[] { -5, 5 };
-            Aquila a = new Aquila(myFuncion: TestsFunctions.Rosenbrock, dim_in: 2, iterations_in: 100, range_in: range, population_size_in: 100,
-                alpha_in: 0.1, beta_in: 1.0, delta_in: 0.1, omega_in: 0.005, D_in: 3, U_in: 0.00565, s_in: 0.01, r1_in: 1);
-            a.Solve();
-            Console.WriteLine(a.FBest);
-            foreach (double e in a.XBest)
+            double[][] range = new double[5][];
+            for (int t = 0; t < 5; t++)
+                range[t] = new double[] { -5.12, 5.12 };
+            double[] xbest = new double[5];
+            double fbest = 0.0;
+            double ooo = 0;
+            for (int j = 0; j < 5; j++)
             {
-                Console.WriteLine(e);
+                xbest[j] = 0.0;
             }
+            int tests = 1000;
+            int ttt = tests;
+            for (int iter = 0; iter < tests; iter++)
+            {
+                Aquila a = new Aquila(myFuncion: TestsFunctions.Beale, dim_in: 2, iterations_in: 100, range_in: range, population_size_in: 100,
+                    alpha_in: 0.1, beta_in: 1.5, delta_in: 0.1, omega_in: 0.005, D_in: 3, U_in: 0.00565, s_in: 0.01, r1_in: 1);
+                a.Solve();
+                fbest += a.FBest;
+                ooo += a.OOO;
+                if (a.OOO <= 0) ttt--;
+                for (int j = 0; j < 2; j++)
+                {
+                    xbest[j] += a.XBest[j];
+                }
+
+            }
+            Console.WriteLine("Arg Fbest from " + tests.ToString()+" tests :" + (fbest / tests).ToString());
+            //Console.WriteLine("OOO: " + (ooo / tests));
+            for (int j = 0; j < 5; j++)
+            {
+                Console.WriteLine("Arg Xbest["+j.ToString()+"] from " + tests.ToString() + " tests :" + (xbest[j] / tests).ToString());
+            }
+
+            //Console.WriteLine(a.FBest);
+            //foreach(double e in a.XBest)
+            {
+                //  Console.WriteLine(e);
+            }
+            Console.Read();
         }
     }
 }
